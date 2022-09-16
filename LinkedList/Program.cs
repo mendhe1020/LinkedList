d90d94f3e8bd928dedaaa42ce3e581d07bf058e6
mendhe1020 <anurag.mendhe14@gmail.com>
2022-09-16 15:23:00 +0530

    [Anurag] Add: Create a Linked List

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Linked List");
+
+            bool flag = true;
+            LinkedList list = new LinkedList();
+            while (flag)
+            {
+                
+                Console.WriteLine("1.Create a Linked List ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        list.Add(56);
+                        list.Add(30);
+                        list.Add(70);
+                        list.Display();
+                        break;
+                }
+            }
         }
     }
 }
